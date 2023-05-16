d235d9f8d9fc79ed3d7c11447f964b5380622594
holdno <wby@ojbk.io>
2023-05-16 15:50:48 +0800

    add proxy metadata

--- infra/client.go
+++ infra/client.go
@@ -5,13 +5,45 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 
 	"github.com/spacegrower/watermelon/infra/resolver"
 	"github.com/spacegrower/watermelon/infra/resolver/etcd"
 )
 
+type ClientConn struct {
+	*grpc.ClientConn
+	md metadata.MD
+}
+
+func (c *ClientConn) JoinMetadata(ctx context.Context) context.Context {
+	if len(c.md) > 0 {
+		var pairs []string
+		for k, vs := range c.md {
+			for _, v := range vs {
+				if v != "" {
+					pairs = append(pairs, k, v)
+				}
+			}
+		}
+		if len(pairs) > 0 {
+			ctx = metadata.AppendToOutgoingContext(ctx, pairs...)
+		}
+	}
+	return ctx
+}
+
+func (cc *ClientConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	return cc.ClientConn.NewStream(cc.JoinMetadata(ctx), desc, method, opts...)
+}
+
+func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply interface{}, opts ...grpc.CallOption) error {
+	return cc.ClientConn.Invoke(cc.JoinMetadata(ctx), method, args, reply, opts...)
+}
+
 type ClientServiceNameGenerator interface {
 	FullServiceName(srvName string) string
+	ProxyMetadata() metadata.MD
 }
 
 type Client[T ClientServiceNameGenerator] struct {
@@ -45,7 +77,7 @@ func WithGrpcDialOptions[T ClientServiceNameGenerator](opts ...grpc.DialOption) 
 	}
 }
 
-func NewClientConn[T ClientServiceNameGenerator](serviceName string, opts ...ClientOptions[T]) (*grpc.ClientConn, error) {
+func NewClientConn[T ClientServiceNameGenerator](serviceName string, opts ...ClientOptions[T]) (*ClientConn, error) {
 	options := &COptions[T]{
 		timeout: time.Second * 5,
 	}
@@ -71,5 +103,8 @@ func NewClientConn[T ClientServiceNameGenerator](serviceName string, opts ...Cli
 		return nil, err
 	}
 
-	return cc, nil
+	return &ClientConn{
+		ClientConn: cc,
+		md:         options.CustomizeMeta.ProxyMetadata().Copy(),
+	}, nil
 }
